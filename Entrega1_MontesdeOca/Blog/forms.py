d6a096a1560b93864466aa1cff9502db88adb2eb
fernando <fernandomontesdeoca93@gmail.com>
2022-10-12 09:30:48 -0300

    models str and forms

---
+++ Entrega1_MontesdeOca/Blog/forms.py
@@ -0,0 +1,21 @@
+from dataclasses import fields
+from pyexpat import model
+from django.forms import ModelForm
+from .models import Autor, Articulo, Seccion
+
+class AutorForm(ModelForm):
+    class Meta:
+        model = Autor
+        fields = ['nombre','apellido','profesion']
+
+class ArticuloForm(ModelForm):
+    class Meta:
+        model = Articulo
+        fields = ['titulo','texto','fecha']
+
+class SeccionForm(ModelForm):
+    class Meta:
+        model = Seccion
+        fields = ['nombre']
+
+
--- Entrega1_MontesdeOca/Blog/models.py
+++ Entrega1_MontesdeOca/Blog/models.py
@@ -6,11 +6,20 @@ class Autor(models.Model):
     apellido = models.CharField(max_length = 40)
     profesion = models.CharField(max_length = 40)
 
+    def __str__(self):
+        return self.apellido
+
 
 class Articulo(models.Model):
     titulo = models.CharField(max_length = 60)
     texto = models.CharField(max_lenght = 500)
     fecha = models.DateField(null = False)
 
+    def __str__(self):
+        return self.titulo
+
 class Seccion(models.model):
     nombre = models.CharField(max_length = 40)
+
+    def __str__(self):
+        return self.nombre
