ef2335efe1fcc011578915ee02d6e6672dd4c19d
fernando <fernandomontesdeoca93@gmail.com>
2022-10-12 00:25:13 -0300

    app model

--- Entrega1_MontesdeOca/Blog/models.py
+++ Entrega1_MontesdeOca/Blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 
+class Autor(models.Model):
+    nombre = models.CharField(max_length = 40)
+    apellido = models.CharField(max_length = 40)
+    profesion = models.CharField(max_length = 40)
+
+
+class Articulo(models.Model):
+    titulo = models.CharField(max_length = 60)
+    texto = models.CharField(max_lenght = 500)
+    fecha = models.DateField(null = False)
+
+class Seccion(models.model):
+    nombre = models.CharField(max_length = 40)
