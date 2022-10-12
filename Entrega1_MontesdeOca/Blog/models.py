b33c0e415282017bb95cb4ed1f5bbdb24f60de24
fernando <fernandomontesdeoca93@gmail.com>
2022-10-12 09:46:59 -0300

    model migration

--- Entrega1_MontesdeOca/Blog/models.py
+++ Entrega1_MontesdeOca/Blog/models.py
@@ -12,13 +12,13 @@ class Autor(models.Model):
 
 class Articulo(models.Model):
     titulo = models.CharField(max_length = 60)
-    texto = models.CharField(max_lenght = 500)
+    texto = models.CharField(max_length = 500)
     fecha = models.DateField(null = False)
 
     def __str__(self):
         return self.titulo
 
-class Seccion(models.model):
+class Seccion(models.Model):
     nombre = models.CharField(max_length = 40)
 
     def __str__(self):
